ad4abe685659fb189bdaeb8ec98d916de0baa9eb
MinalMahalaShorthillsAI <minal@shorthills.ai>
2024-09-13 10:02:40 +0530

    First commit

---
+++ Practice.py
@@ -0,0 +1,14 @@
+print("minal")
+print("Hello, My name is Minal Mahala")        
+def Kite():
+    """This is a function for returning a greetings.
+
+    Returns
+    -------
+    _type_
+        String
+    """
+    return ("hello")
+ans=Kite()
+# adding comments 
+print(ans)
